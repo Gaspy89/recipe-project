6972febc96ec823231946b63616055c03e8b076c
Marton Gaspardy <gaspy89@gmail.com>
2020-12-09 12:15:48 +0100

    Initial commit

---
+++ src/test/java/com/gaspy/recipeproject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaspy.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
