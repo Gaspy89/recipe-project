a8ee8f9e15290c1777e60798d653b9fcf7d6d80a
Marton Gaspardy <gaspy89@gmail.com>
2020-12-11 12:46:59 +0100

    added difficulty enum

---
+++ src/main/java/com/gaspy/recipeproject/model/Difficulty.java
@@ -0,0 +1,5 @@
+package com.gaspy.recipeproject.model;
+
+public enum Difficulty {
+    EASY,MODERATE,HARD
+}
--- src/main/java/com/gaspy/recipeproject/model/Recipe.java
+++ src/main/java/com/gaspy/recipeproject/model/Recipe.java
@@ -15,8 +15,10 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //Private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
     @Lob
@@ -111,5 +113,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
