e0f27857afef7640b8f8678d5a4173ea0894bc8f
Marton Gaspardy <gaspy89@gmail.com>
2020-12-19 14:50:39 +0100

    added mock mvc

--- src/test/java/com/gaspy/recipeproject/controllers/IndexControllerTest.java
+++ src/test/java/com/gaspy/recipeproject/controllers/IndexControllerTest.java
@@ -1,23 +1,22 @@
 package com.gaspy.recipeproject.controllers;
 
 import com.gaspy.recipeproject.model.Recipe;
-import com.gaspy.recipeproject.repositories.RecipeRepository;
 import com.gaspy.recipeproject.services.RecipeService;
-import com.gaspy.recipeproject.services.RecipeServiceImpl;
 import org.junit.jupiter.api.*;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
-
 import java.util.HashSet;
 import java.util.Set;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class IndexControllerTest {
 
@@ -33,6 +32,12 @@ class IndexControllerTest {
     void setUp() {
         MockitoAnnotations.initMocks(this);
         indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
     }
 
     @Test
