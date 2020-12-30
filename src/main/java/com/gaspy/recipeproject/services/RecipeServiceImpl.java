1b7543a19d870a5eb957e39632fd28c056566997
Marton Gaspardy <gaspy89@gmail.com>
2020-12-30 14:18:09 +0100

    added show recipe feature

--- src/main/java/com/gaspy/recipeproject/services/RecipeServiceImpl.java
+++ src/main/java/com/gaspy/recipeproject/services/RecipeServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -24,5 +25,14 @@ public class RecipeServiceImpl implements RecipeService{
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().forEach(recipes::add);
         return recipes;
+    }
+
+    @Override
+    public Recipe findById(Long id) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+        if(!recipeOptional.isPresent()) {
+            throw new RuntimeException("No recipe is found");
+        }
+        return recipeOptional.get();
     }
 }
